574fb31dc88300b7fc75fbb07fe9c2733dae9d40
ngphthao10 <n21dccn078@student.ptithcm.edu.vn>
2024-06-19 23:31:44 +0700

    form chuyen khoa

---
+++ QLDSV_TC/frmChuyenKhoa.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace QLDSV_TC
+{
+    partial class frmChuyenKhoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label tENLOPLabel;
+            System.Windows.Forms.Label label2;
+            this.btnPreview = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.txtMaLop = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.cmbTenLop = new Siticone.Desktop.UI.WinForms.SiticoneComboBox();
+            this.bdsLop = new System.Windows.Forms.BindingSource(this.components);
+            this.QLDSV_TCDataSet = new QLDSV_TC.QLDSV_TCDataSet();
+            this.cmbKhoa = new Siticone.Desktop.UI.WinForms.SiticoneComboBox();
+            this.txtMaSV = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.bdsSV = new System.Windows.Forms.BindingSource(this.components);
+            this.SINHVIENTableAdapter = new QLDSV_TC.QLDSV_TCDataSetTableAdapters.SINHVIENTableAdapter();
+            this.tableAdapterManager = new QLDSV_TC.QLDSV_TCDataSetTableAdapters.TableAdapterManager();
+            this.LOPTableAdapter = new QLDSV_TC.QLDSV_TCDataSetTableAdapters.LOPTableAdapter();
+            label1 = new System.Windows.Forms.Label();
+            tENLOPLabel = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsLop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QLDSV_TCDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.Location = new System.Drawing.Point(68, 70);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(53, 23);
+            label1.TabIndex = 24;
+            label1.Text = "Khoa:";
+            // 
+            // tENLOPLabel
+            // 
+            tENLOPLabel.AutoSize = true;
+            tENLOPLabel.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            tENLOPLabel.Location = new System.Drawing.Point(68, 124);
+            tENLOPLabel.Name = "tENLOPLabel";
+            tENLOPLabel.Size = new System.Drawing.Size(69, 23);
+            tENLOPLabel.TabIndex = 23;
+            tENLOPLabel.Text = "Tên lớp:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label2.Location = new System.Drawing.Point(68, 177);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(63, 23);
+            label2.TabIndex = 27;
+            label2.Text = "Mã SV:";
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.BorderRadius = 10;
+            this.btnPreview.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnPreview.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnPreview.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnPreview.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnPreview.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPreview.ForeColor = System.Drawing.Color.White;
+            this.btnPreview.Location = new System.Drawing.Point(189, 243);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(280, 50);
+            this.btnPreview.TabIndex = 25;
+            this.btnPreview.Text = "Lưu thông tin";
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // txtMaLop
+            // 
+            this.txtMaLop.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(221)))), ((int)(((byte)(226)))));
+            this.txtMaLop.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtMaLop.DefaultText = "";
+            this.txtMaLop.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtMaLop.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtMaLop.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMaLop.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMaLop.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMaLop.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaLop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.txtMaLop.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMaLop.Location = new System.Drawing.Point(444, 118);
+            this.txtMaLop.Name = "txtMaLop";
+            this.txtMaLop.PasswordChar = '\0';
+            this.txtMaLop.PlaceholderText = "";
+            this.txtMaLop.ReadOnly = true;
+            this.txtMaLop.SelectedText = "";
+            this.txtMaLop.Size = new System.Drawing.Size(138, 36);
+            this.txtMaLop.TabIndex = 22;
+            // 
+            // cmbTenLop
+            // 
+            this.cmbTenLop.BackColor = System.Drawing.Color.Transparent;
+            this.cmbTenLop.DataSource = this.bdsLop;
+            this.cmbTenLop.DisplayMember = "TENLOP";
+            this.cmbTenLop.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbTenLop.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTenLop.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbTenLop.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbTenLop.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbTenLop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cmbTenLop.ItemHeight = 30;
+            this.cmbTenLop.Location = new System.Drawing.Point(158, 118);
+            this.cmbTenLop.Name = "cmbTenLop";
+            this.cmbTenLop.Size = new System.Drawing.Size(280, 36);
+            this.cmbTenLop.TabIndex = 21;
+            this.cmbTenLop.ValueMember = "MALOP";
+            this.cmbTenLop.SelectedIndexChanged += new System.EventHandler(this.cmbTenLop_SelectedIndexChanged);
+            // 
+            // bdsLop
+            // 
+            this.bdsLop.DataMember = "LOP";
+            this.bdsLop.DataSource = this.QLDSV_TCDataSet;
+            // 
+            // QLDSV_TCDataSet
+            // 
+            this.QLDSV_TCDataSet.DataSetName = "QLDSV_TCDataSet";
+            this.QLDSV_TCDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cmbKhoa
+            // 
+            this.cmbKhoa.BackColor = System.Drawing.Color.Transparent;
+            this.cmbKhoa.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbKhoa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbKhoa.Enabled = false;
+            this.cmbKhoa.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbKhoa.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbKhoa.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbKhoa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.cmbKhoa.ItemHeight = 30;
+            this.cmbKhoa.Location = new System.Drawing.Point(158, 64);
+            this.cmbKhoa.Margin = new System.Windows.Forms.Padding(5);
+            this.cmbKhoa.Name = "cmbKhoa";
+            this.cmbKhoa.Size = new System.Drawing.Size(424, 36);
+            this.cmbKhoa.TabIndex = 20;
+            this.cmbKhoa.SelectedIndexChanged += new System.EventHandler(this.cmbKhoa_SelectedIndexChanged);
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(221)))), ((int)(((byte)(226)))));
+            this.txtMaSV.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtMaSV.DefaultText = "";
+            this.txtMaSV.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtMaSV.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtMaSV.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMaSV.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMaSV.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMaSV.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaSV.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.txtMaSV.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMaSV.Location = new System.Drawing.Point(158, 171);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.PasswordChar = '\0';
+            this.txtMaSV.PlaceholderText = "";
+            this.txtMaSV.ReadOnly = true;
+            this.txtMaSV.SelectedText = "";
+            this.txtMaSV.Size = new System.Drawing.Size(424, 36);
+            this.txtMaSV.TabIndex = 26;
+            // 
+            // bdsSV
+            // 
+            this.bdsSV.DataMember = "SINHVIEN";
+            this.bdsSV.DataSource = this.QLDSV_TCDataSet;
+            // 
+            // SINHVIENTableAdapter
+            // 
+            this.SINHVIENTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CT_DONGHOCPHITableAdapter = null;
+            this.tableAdapterManager.DANGKYTableAdapter = null;
+            this.tableAdapterManager.GIANGVIENTableAdapter = null;
+            this.tableAdapterManager.HOCPHITableAdapter = null;
+            this.tableAdapterManager.KHOATableAdapter = null;
+            this.tableAdapterManager.LOPTableAdapter = this.LOPTableAdapter;
+            this.tableAdapterManager.LOPTINCHITableAdapter = null;
+            this.tableAdapterManager.MONHOCTableAdapter = null;
+            this.tableAdapterManager.SINHVIENTableAdapter = this.SINHVIENTableAdapter;
+            this.tableAdapterManager.UpdateOrder = QLDSV_TC.QLDSV_TCDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // LOPTableAdapter
+            // 
+            this.LOPTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmChuyenKhoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 342);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.txtMaSV);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(label1);
+            this.Controls.Add(tENLOPLabel);
+            this.Controls.Add(this.txtMaLop);
+            this.Controls.Add(this.cmbTenLop);
+            this.Controls.Add(this.cmbKhoa);
+            this.Name = "frmChuyenKhoa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chuyển khoa";
+            this.Load += new System.EventHandler(this.frmChuyenKhoa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bdsLop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QLDSV_TCDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsSV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticoneButton btnPreview;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtMaLop;
+        private Siticone.Desktop.UI.WinForms.SiticoneComboBox cmbTenLop;
+        private Siticone.Desktop.UI.WinForms.SiticoneComboBox cmbKhoa;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtMaSV;
+        private QLDSV_TCDataSet QLDSV_TCDataSet;
+        private System.Windows.Forms.BindingSource bdsSV;
+        private QLDSV_TCDataSetTableAdapters.SINHVIENTableAdapter SINHVIENTableAdapter;
+        private QLDSV_TCDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private QLDSV_TCDataSetTableAdapters.LOPTableAdapter LOPTableAdapter;
+        private System.Windows.Forms.BindingSource bdsLop;
+    }
+}
---
+++ QLDSV_TC/frmChuyenKhoa.cs
@@ -0,0 +1,138 @@
+﻿using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLDSV_TC
+{
+    public partial class frmChuyenKhoa : Form
+    {
+        private string masv = "";
+        private string maLop = "";
+        public frmChuyenKhoa(string masv)
+        {
+            InitializeComponent();
+            this.masv = masv;
+        }
+        private bool CheckData()
+        {
+            if (txtMaLop.Text == null)
+            {
+                MessageBox.Show("Lớp không được bỏ trống", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void frmChuyenKhoa_Load(object sender, EventArgs e)
+        {
+            QLDSV_TCDataSet.EnforceConstraints = false;
+            this.SINHVIENTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.SINHVIENTableAdapter.Fill(this.QLDSV_TCDataSet.SINHVIEN);
+            this.LOPTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.LOPTableAdapter.Fill(this.QLDSV_TCDataSet.LOP);
+            Program.bds_dspm.Filter = "TENPM not LIKE '%Thông tin đóng học phí%'";
+            cmbKhoa.DataSource = Program.bds_dspm;
+            cmbKhoa.DisplayMember = "TENPM";
+            cmbKhoa.ValueMember = "TENSERVER";
+            cmbKhoa.SelectedIndex = Program.mPhanManh;
+
+            if (Program.mGroup == "PGV")
+                cmbKhoa.Enabled = true;
+
+            cmbTenLop.SelectedIndex = 0;
+            txtMaLop.Text = ("" + cmbTenLop.SelectedValue).Replace(" ", "");
+            txtMaSV.Text = masv;
+        }
+
+        private void cmbTenLop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txtMaLop.Text = ("" + cmbTenLop.SelectedValue).Replace(" ", "");
+                maLop = txtMaLop.Text;
+            }
+            catch (Exception ex) { }
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            DataTable sinhVienTable = SINHVIENTableAdapter.GetData();
+            DataRow[] sinhVienRows = sinhVienTable.Select("masv = '" + txtMaSV.Text + "'");
+
+           
+            string hoTen = sinhVienRows[0]["HO"].ToString() + " " + sinhVienRows[0]["TEN"].ToString();
+            string malop = sinhVienRows[0]["MALOP"].ToString();
+
+            if (CheckData())
+            {
+
+                DialogResult dialog =
+                    MessageBox.Show("Bạn cần kiểm tra trước thực hiện chuyển đổi:" +
+                    "\n\nMã số sinh viên  : " + txtMaSV.Text +
+                    "\n\nHọ và tên        : " + hoTen +
+                    "\n\nMã lớp hiện tại  : " + malop +
+                    "\n\nMã lớp chuyển tới: " + txtMaLop.Text
+                    , "Thông báo!", MessageBoxButtons.YesNo);
+
+                if (dialog == DialogResult.Yes)
+                {
+                    if (Program.conn.State == ConnectionState.Closed) Program.conn.Open();
+
+                    String strLenh = "EXEC dbo.SP_CHUYENLOP_SV '" + masv + "', '"  + txtMaLop.Text.Trim() + "'";
+                    System.Diagnostics.Debug.Print(strLenh);
+
+                    int result = Program.ExecSqlNonQuery(strLenh);
+
+                    if (result != 0)
+                    {
+                        MessageBox.Show("Đã có lỗi xảy ra khi thực thi thủ tục lưu trữ.");
+                        return;
+                    }
+                    Program.conn.Close();
+                    MessageBox.Show("Thực hiện chuyển đổi lớp thành công!", "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Thao tác chuyển đổi đã được hủy!", "Thông báo!", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void cmbKhoa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbKhoa.SelectedValue.ToString() == "System.Data.DataRowView")
+                return;
+            Program.servername = cmbKhoa.SelectedValue.ToString();
+
+            if (cmbKhoa.SelectedIndex != Program.mPhanManh)
+            {
+                Program.mlogin = Program.remotelogin;
+                Program.password = Program.remotepassword;
+            }
+            else
+            {
+                Program.mlogin = Program.mloginDN;
+                Program.password = Program.passwordDN;
+            }
+
+            if (Program.KetNoi() == 0)
+            {
+                MessageBox.Show("Lỗi kết nối về chi nhánh mới", "", MessageBoxButtons.OK);
+                return;
+            }
+            else
+            {
+                this.LOPTableAdapter.Connection.ConnectionString = Program.connstr;
+                this.LOPTableAdapter.Fill(this.QLDSV_TCDataSet.LOP);
+            }
+        }
+    }
+}
